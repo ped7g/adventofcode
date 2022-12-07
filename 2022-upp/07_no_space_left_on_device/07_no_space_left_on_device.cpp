7ad8c05eef98bbe546d7c7cfe1e6952a6fde3830
Peter Helcmanovsky (Ped) <ped@7gods.org>
2022-12-07 18:07:59 +0100

    AoC 2022: day 7 refactored solution, added protection against duplicit-ls

--- 2022-upp/07_no_space_left_on_device/07_no_space_left_on_device.cpp
+++ 2022-upp/07_no_space_left_on_device/07_no_space_left_on_device.cpp
@@ -22,169 +22,111 @@ void lines_loop(T task, const String & filename) {
 	task.finish();
 }
 
-struct AocFile : Moveable<AocFile>  {
-	String p;	// full path
-	int64 s;		// size
-
-	AocFile(const String & path, const String & name, int64 size) :
-		p(path + name), s(size) {}
-
-	bool operator <(const AocFile & b) const {
-		//Cout() << "compare " << p << " vs " << b.p << " = " << p.Compare(b.p) << EOL;
-		return p.Compare(b.p) < 0;
-	}
-
-	String ToString() const {
-		return Format("%s %d", p, Value(s));
-	}
-};
-
 static ConvertInt64 stoi64;
 
-class Part1 {	// expected answers: sample 95437, input 1886043
+struct AocFile : Moveable<AocFile>  {
+	String p;		// full path
+	int64 s;		// size
 
-	String pwd_;
-	Vector<AocFile> files_;
+	AocFile(const String & path, const String & name, int64 size)
+		: p(path + "/" + name), s(size) {}
 
-public:
-
-	void init() { Cout() << "part1"; }
-
-	bool line(const String & line) {
-		if (line.IsEmpty()) return true;
-		auto words = Split(line, ' ');
-		if ('$' == line[0]) {
-			if (0 == words[1].Compare("cd")) {
-				if (0 == words[2].Compare("/")) {
-					pwd_ = "/";
-				} else if (0 == words[2].Compare("..")) {
-					int pd = pwd_.ReverseFind('/', pwd_.GetLength() - 2);
-					if (pd >= 0) pwd_.TrimLast(pwd_.GetLength() - pd - 1);
-				} else {
-					pwd_.Cat(words[2]);
-					pwd_.Cat("/");
-				}
-			} else if (0 == words[1].Compare("ls")) {
-				// Cout() << "ls " << pwd_ << EOL;
-			}
-		} else if (0 == words[0].Compare("dir")) {
-		} else {
-			files_.Add(AocFile(pwd_, words[1], stoi64.Scan(words[0])));
-		}
-		return false;							// not finished yet, try next line
-	}
-
-	void finish() {
-		// sort files to make "group by path" algorithm possible
-		Sort(files_);
-		// sum all folders in "group by folder" way with cummulative nesting
-		Vector<int64> sizes;
-		String pwd = "/";
-		int64 sz = 0, at_most_100k_sum = 0;
-		files_.Add(AocFile("/", "", 0));		// fake empty file to close summing of root size
-		for (const auto & f : files_) {
-			while (!f.p.StartsWith(pwd)) {
-				//Cout() << pwd << " = " << sz << EOL;
-				if (sz <= 100000) at_most_100k_sum += sz;
-				int slash = pwd.ReverseFind('/', pwd.GetLength() - 2);
-				if (slash < 0) break;
-				pwd.Trim(slash + 1);
-				sz += sizes.Pop();
-			}
-			int nextdir, pwdl;
-			while (0 <= (pwdl = pwd.GetLength(), nextdir = f.p.Find('/', pwdl))) {
-				sizes.Add(sz);
-				pwd.Cat(f.p.Mid(pwdl, nextdir - pwdl + 1));
-				sz = 0;
-			}
-			sz += f.s;
-		}
-		ASSERT(sizes.IsEmpty());
-		Cout() << pwd << " = " << sz << EOL;
-		Cout() << "part1: " << at_most_100k_sum << EOL;
-	}
+	// for sort of files by full path
+	int operator <=> (const AocFile & b) const { return p.Compare(b.p); }
 };
 
-class Part2 {	// expected answers: sample 24933642, input 3842121
+class BothParts {
 
-	static constexpr int64 FS_SIZE = 70000000;
-	static constexpr int64 UPDATE_REQ = 30000000;
+	static constexpr int64 FS_SIZE		= 70'000'000;
+	static constexpr int64 UPDATE_REQ	= 30'000'000;
 
-	String pwd_;
+	String parse_dir_;
+	bool ignore_listed_files_ = true;
+	Index<String> listed_;
 	Vector<AocFile> files_;
 
 public:
 
-	void init() { Cout() << "part2"; }
+	void init() { Cout() << "parsing"; }
 
 	bool line(const String & line) {
-		if (line.IsEmpty()) return true;
+		if (line.IsEmpty()) return true;		// invalid input
 		auto words = Split(line, ' ');
-		if ('$' == line[0]) {
-			if (0 == words[1].Compare("cd")) {
-				if (0 == words[2].Compare("/")) {
-					pwd_ = "/";
-				} else if (0 == words[2].Compare("..")) {
-					int pd = pwd_.ReverseFind('/', pwd_.GetLength() - 2);
-					if (pd >= 0) pwd_.TrimLast(pwd_.GetLength() - pd - 1);
-				} else {
-					pwd_.Cat(words[2]);
-					pwd_.Cat("/");
+		if ('$' == line[0]) {					// user command
+			ignore_listed_files_ = true;		// ignore any unexpected listing output
+			if (!words[1].Compare("cd")) {
+				// CD command, keep track of current directory (in parse_dir_ var)
+				if (!words[2].Compare("/")) {
+					parse_dir_.Clear();
+				} else if (!words[2].Compare("..")) {	// remove last directory from parse_dir_
+					int pd = parse_dir_.ReverseFind('/');	// find parent dir
+					if (pd >= 0) parse_dir_.TrimLast(parse_dir_.GetLength() - pd);
+				} else {								// append new directory to parse_dir_
+					parse_dir_.Cat('/');
+					parse_dir_.Cat(words[2]);
 				}
-			} else if (0 == words[1].Compare("ls")) {
-				// Cout() << "ls " << pwd_ << EOL;
+			} else if (!words[1].Compare("ls")) {
+				// protect against duplicate ls (not part of AoC inputs)
+				ignore_listed_files_ = (0 <= listed_.Find(parse_dir_));
+				listed_.Add(parse_dir_);
 			}
-		} else if (0 == words[0].Compare("dir")) {
-		} else {
-			files_.Add(AocFile(pwd_, words[1], stoi64.Scan(words[0])));
+		} else if (!words[0].Compare("dir")) {	// listing of sub-directory
+			// nothing to do, files are stored with full paths as flat vector, not using FS tree
+		} else {								// file with size, add it to files_
+			if (!ignore_listed_files_) {
+				files_.Add(AocFile(parse_dir_, words[1], stoi64.Scan(words[0])));
+			}
 		}
 		return false;							// not finished yet, try next line
 	}
 
 	void finish() {
-		// sort files to make "group by path" algorithm possible
+		// sort files to make "group by directory" algorithm possible
 		Sort(files_);
-		// sum all folders in "group by folder" way with cummulative nesting
-		Vector<int64> dir_sizes;
-		Vector<int64> sizes;
-		String pwd = "/";
-		int64 sz = 0;
-		files_.Add(AocFile("/", "", 0));		// fake empty file to close summing of root size
+		// sum all files in "group by directory" way with cummulative nesting
+		int64 dir_size = 0, at_most_100k_sum = 0;	// sum of <= 100k dirs (part 1)
+		Vector<int64> dir_sizes;				// list of all possible directory sizes (part 2)
+		Vector<int64> sizes;					// stack to hold size of parent directories
+		String group_by = "/";
+		files_.Add(AocFile("", "", 0));			// fake "/" file to finish summing of last dir
 		for (const auto & f : files_) {
-			while (!f.p.StartsWith(pwd)) {
-				//Cout() << pwd << " = " << sz << EOL;
-				dir_sizes.Add(sz);
-				int slash = pwd.ReverseFind('/', pwd.GetLength() - 2);
+			while (!f.p.StartsWith(group_by)) {	// next file has different dir, finish group
+				if (dir_size <= 100'000) at_most_100k_sum += dir_size;	// sum part 1 result
+				dir_sizes.Add(dir_size);		// record dir size for part 2 result
+				int slash = group_by.ReverseFind('/', group_by.GetLength() - 2);
 				if (slash < 0) break;
-				pwd.Trim(slash + 1);
-				sz += sizes.Pop();
+				group_by.Trim(slash + 1);
+				dir_size += sizes.Pop();		// update parent dir size with closed dir size
 			}
-			int nextdir, pwdl;
-			while (0 <= (pwdl = pwd.GetLength(), nextdir = f.p.Find('/', pwdl))) {
-				sizes.Add(sz);
-				pwd.Cat(f.p.Mid(pwdl, nextdir - pwdl + 1));
-				sz = 0;
+			int next_dir, gbl;
+			while (0 <= (gbl = group_by.GetLength(), next_dir = f.p.Find('/', gbl))) {
+				sizes.Add(dir_size);			// preserve current dir size as new parent dir
+				group_by = f.p.Left(next_dir + 1);		// update group_by to include new dir
+				dir_size = 0;					// reset current dir size to zero
 			}
-			sz += f.s;
+			dir_size += f.s;					// add file size to current directory sum
 		}
 		ASSERT(sizes.IsEmpty());
-		int64 to_free = UPDATE_REQ - (FS_SIZE - sz);
-		Cout() << pwd << " = " << sz << " to free: " << to_free << EOL;
+		// all directories summed, find the smallest one to delete to have enough free space
+		int64 to_free = UPDATE_REQ - (FS_SIZE - dir_size);
+		Cout() << group_by << " = " << dir_size << " to free: " << to_free << EOL;	// debug
 		// find the smallest directory size which will free enough space
 		int64 min_dir = INT64_MAX;
-		for (const auto dsz : dir_sizes) {
-			if (dsz < to_free) continue;
-			min_dir = std::min(min_dir, dsz);
+		for (const auto size : dir_sizes) {
+			if (size < to_free) continue;
+			min_dir = std::min(min_dir, size);
 		}
+		// display results
+		Cout() << "part1: " << at_most_100k_sum << EOL;
 		Cout() << "part2: " << min_dir << EOL;
 	}
 };
 
+// expected answers: sample [95437, 24933642], input [1886043, 3842121], tricky [95437, 24933642]
 CONSOLE_APP_MAIN
 {
 	const Vector<String>& cmdline = CommandLine();
 	for(int i = 0; i < cmdline.GetCount(); i++) {
-		lines_loop(Part1(), cmdline[i]);
-		lines_loop(Part2(), cmdline[i]);
+		lines_loop(BothParts(), cmdline[i]);
 	}
 }
