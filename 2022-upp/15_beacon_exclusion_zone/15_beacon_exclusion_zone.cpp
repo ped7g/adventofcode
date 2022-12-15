7cf6e8e23c136e2a4816baf94c8f0fb3573d075c
Peter Helcmanovsky (Ped) <ped@7gods.org>
2022-12-15 11:51:49 +0100

    AoC 2022: day 15 - fix part 2 bug, code cleanup (same algorithm)
    part 2 would miss distress beacon, if it would be at x=0, and there
    would be no sensor coverage ahead, then the check would start at x=1 and
    miss the uncovered spot at x=0.

--- 2022-upp/15_beacon_exclusion_zone/15_beacon_exclusion_zone.cpp
+++ 2022-upp/15_beacon_exclusion_zone/15_beacon_exclusion_zone.cpp
@@ -2,50 +2,63 @@
 
 using namespace Upp;
 
+struct Position : Moveable<Position> {
+	int x, y;
+
+	int manhattan_d(const Position & b) const {
+		return abs(x - b.x) + abs(y - b.y);
+	}
+};
+
+struct Span : Moveable<Span> {
+	int from, to;
+
+	Span(int f, int t) : from(f), to(t) {}
+
+	static bool less(const Span & a, const Span & b) {
+		if (a.from == b.from) return b.to < a.to;			// longer span for same "from"
+		return a.from < b.from;								// order spans by "from"
+	}
+};
+
 class Part1 {
 
-	int p1_y = 0;
-	Vector<Tuple<int, int>> p1_spans;
-	Index<int> p1_beacons;
+	int p1_y = 0;				// *the* part 1 answer line Y (read from patched input file)
+	Vector<Span> spans;			// [close, open) spans of coverage at line Y
+	Index<int> beacons;			// all unique beacons (X coordinates) at line Y
 
 public:
 
 	void init() { Cout() << "part1"; }
 
 	bool line(const String & line) {
-		int Sx, Sy, Bx, By;
+		Position s, b;
 		if (4 != sscanf(~line, "Sensor at x=%d, y=%d: closest beacon is at x=%d, y=%d",
-						&Sx, &Sy, &Bx, &By)) {
-			return (1 != sscanf(~line, "%d", &p1_y));
+						&s.x, &s.y, &b.x, &b.y)) {
+			return (1 != sscanf(~line, "%d", &p1_y));		// custom input: first line = Y
 		}
-		if (p1_y == By) p1_beacons.FindAdd(Bx);
-		int dx = abs(Bx-Sx) + abs(By-Sy), dy = abs(p1_y-Sy);
-		//Sensor at x=8, y=7: closest beacon is at x=2, y=10
-		// dx=6+3 = 9, dy=3 (line 10), line 16: dx=9, dy=9
-		// span -> Sx-(dx-dy), Sx+(dx-dy)
-		//		   8-(10-4) = 2, 8+(10-4) = 14,   8-(10-10) = 8, 8+(10-10) = 8
-		int p1_line_power = dx - dy;
-		if (p1_line_power < 0) return false;
-		p1_spans.Add({Sx - p1_line_power, Sx + p1_line_power});
-		return false;							// not finished yet, try next line
+		if (p1_y == b.y) beacons.FindAdd(b.x);				// track all unique beacons at Y
+		int line_power = b.manhattan_d(s) - abs(p1_y-s.y);	// sensor X-range at Y
+		if (0 <= line_power) spans.Add({s.x - line_power, s.x + line_power + 1});
+		return false;										// not finished yet, try next line
 	}
 
 	void finish() {
-		Sort(p1_spans, [] (const Tuple<int, int> & a, const Tuple<int, int> & b) -> bool { return a.a < b.a; });
-		int p1_pos = -p1_beacons.GetCount(), x = p1_spans.IsEmpty() ? 0 : p1_spans[0].a;
-		for (auto & s : p1_spans) {
-			if (x < s.a) x = s.a;
-			if (x <= s.b) p1_pos += 1 + (s.b - x), x = s.b + 1;
+		Sort(spans, Span::less);
+		int covered_pos = -beacons.GetCount(), x = spans.IsEmpty() ? 0 : spans[0].from;
+		for (auto & s : spans) {
+			if (x < s.from) x = s.from;						// skip uncovered area
+			if (x < s.to) covered_pos += s.to - x, x = s.to;
 		}
-		Cout() << "part1: " << p1_pos << EOL;
+		Cout() << "part1: " << covered_pos << EOL;
 	}
 };
 
 class Part2 {
 
-	int p1_y = 0, p2_sz = 0;
-	Vector<Tuple<int, int, int, int>> input;
-	Vector<Tuple<int, int>> spans;
+	int p1_y = 0, p2_sz = 0;	// part 2 size of area of interest
+	Vector<Tuple<Position, Position>> input;
+	Vector<Span> spans;
 	Index<int> beacons;
 
 public:
@@ -53,36 +66,37 @@ public:
 	void init() { Cout() << "part2"; }
 
 	bool line(const String & line) {
-		int Sx, Sy, Bx, By;
+		Position s, b;
 		if (4 != sscanf(~line, "Sensor at x=%d, y=%d: closest beacon is at x=%d, y=%d",
-						&Sx, &Sy, &Bx, &By)) {
+						&s.x, &s.y, &b.x, &b.y)) {
+			// custom input: first line: part1_y part2_area_size
 			return (2 != sscanf(~line, "%d %d", &p1_y, &p2_sz));
 		}
-		input.Add({Sx, Sy, Bx, By});
-		return false;							// not finished yet, try next line
+		input.Add({s, b});		// just record all sensor + beacon positions
+		return false;
 	}
 
 	void finish() {
 		for (int ly = 0; ly <= p2_sz; ++ly) {
 			spans.Clear(), beacons.Clear();
 			for (const auto & i : input) {
-				if (ly == i.d) beacons.FindAdd(i.c);
-				int dx = abs(i.c-i.a) + abs(i.d-i.b), dy = abs(ly-i.b);
-				int line_power = dx - dy;
-				if (0 <= line_power) spans.Add({i.a - line_power, i.a + line_power});
+				if (ly == i.b.y) beacons.FindAdd(i.b.x);	// track all unique beacons at ly
+				int line_power = i.b.manhattan_d(i.a) - abs(ly-i.a.y);	// sensor X-range at ly
+				if (0 <= line_power) spans.Add({i.a.x - line_power, i.a.x + line_power + 1});
 			}
-			Sort(spans, [] (const Tuple<int, int> & a, const Tuple<int, int> & b) -> bool { return a.a < b.a; });
-			int x = spans.IsEmpty() ? 0 : std::max(0, spans[0].a);
-			for (const auto & s : spans) {
-				if (p2_sz < x) break;
-				while (x < s.a) {
-					Cout() << Format("part2 beacons free at [%d,%d] = ", x, ly) << 4000000ULL * x + ly << EOL;
+			ASSERT(!spans.IsEmpty());						// this would cause 4mil of output
+			Sort(spans, Span::less);
+			int x = 0;
+			for (const auto & s : spans) {					// look for uncovered position
+				if (p2_sz < x) break;						// whole range 0..p2_sz checked
+				while (x < s.from) {			// uncovered position detected, print it out
+					Cout() << Format("part2 beacon can be at [%d,%d] = %lld", x, ly, 4000000LL * x + ly) << EOL;
 					++x;
 				}
-				if (x <= s.b) x = s.b + 1;
+				if (x < s.to) x = s.to;
 			}
 		}
-		Cout() << "part2 finished" << EOL;
+		Cout() << "part2 finished (full scan)" << EOL;
 	}
 };
 
